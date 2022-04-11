de9a5f8af48f98cd6b170ab80b620414dd9c3197
jeong_yo <dywjd3045@gmail.com>
2022-04-11 20:26:29 +0900

    init

---
+++ src/Components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Title from '../Components/Title';
+
+function Title() {
+    return (
+        <>
+            <h1>투두리스트</h1>
+        </>
+    );
+};
+
+
+
+export default Title;
