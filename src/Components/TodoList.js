8dc14f68da965a7501f39c8c8dfed57934d260f3
jeong_yo <dywjd3045@gmail.com>
2022-04-15 20:39:20 +0900

    배열 버그

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -4,99 +4,93 @@ import "../style/Complete.css";
 function TodoList() { 
     //1.TodoList 시작
             // --console.log("1");
-
+    // const [a, setA] = useState(1)
     const [ tdInput, setTdInput ] = useState("");
-    //3. useState는 tdInput(상태유지값) 과 setTdInput(값을 갱신하는 함수) 을 반환하여 초기값이 ""로>빈공간으로 찍혀나온다.
-            // --console.log("2");
-    const [todos, setTodos] = useState([]);
-    //5. useState는 todos(상태유지값) 과 useState(값을 갱신하는 함수) 를 반환하여 초기값이 []로> 배열로 찍혀나온다.
-            // --console.log("3");
+    const [todos, setTodos] = useState([]); //투두리스트의 배열
+    const [todoIndex, setTodoIdex] = useState(); //투두리스트 배열의 순서
+    console.log(todos)
 
     function inputChange(event) {
-        //7. input 태그에 텍스트를 입력하면 변화가 일어나며 이벤트함수가 발동된다.
-            // console.log("4");
-        
         setTdInput(event.target.value);
-        //9. inputChange 이벤트가 일어난 타겟의 value를 setTdInput에 넣어준다.
-        //10. tdInput에 값이 변경된다.?(덮어진다?)
-            // --console.log("5");
+        // setA(99999999)
     };
    
     function addTodoBtn(event) {
-        //12. 추가 버튼을 클릭하면 addTodoBtn 이벤트함수가 발동된다.
-            // --console.log("6");
 
-            // 기존값을 들고 있다. 
         setTodos(todos => [...todos, tdInput]);
-        //13. todos 를 펼치고 tdInput의 값고 같이 배열을 만들어준다.
-            // --console.log("7");
         setTdInput("");
-        //14. setTdInput의 값을 비워준다.
-            // --console.log("8");
     };
-            // console.log("9");
 
     function deleteTodoBtn(todo) {
-        //15. deleteTodoBtn 버튼을 클릭하면 이벤트함수가 발동된다.
-            // --console.log("10");
+
+
+        // debugger;
+        // todos의 배열안에서 
+        // const todoLenth = todo.length;
+        // for (todoLenth = todo.length; todoLenth++1) 
+        // console.log(todo);
+        // console.log(todo.length);
+
+        setTodoIdex(todo.target.index);
+        console.log(setTodoIdex);
+        // 선택한 delete 버튼의 순번을 찾고 
+        // const todosIndex = todos.index;
+        // todos의 배열에서 
+
+        // const sameTodoIndex = setTodoIdex.value(value => value===todosIndex);
+        // 해당하는 순번을 찾아 
+        // 순번이 일치할 경우 
+        // 제거를 해준다.
+
+        // updatedTodos.splice(targetIndex, 1);
+
+
+
+
 
         const targetIndex = todos.findIndex(value => value === todo);
-        //16. todos의 배열의 순서와 todo 배열의 순서가 일치하면 targetIndex에 값을 넣는다.
-            // --console.log("11");
+        console.log(targetIndex);
 
         const updatedTodos = [...todos]; 
-        //17. todos를 펼쳐 배열로 묶어준다.
-            // --console.log("12");
 
         updatedTodos.splice(targetIndex, 1);
-        //18. updatedTodos 배열에서 targetIndex의 값에 표출된 순서의 요소를 한개 제거 한다.
-            // --console.log("13");
 
         setTodos(updatedTodos);
-        //19. updatedTodos값을 setTodos값에 넣어준다.
-        //20. todos에 값이 적용
-            // --console.log("14");
     };
 
-
-    // 이동
-    // const todoComplete = querySelector("#todoCompleteId");
-    // console.log("15");
-
-    // function moveCompleteBtn(todo) {
-    //     console.log("16");
-    //     todoComplete = [...todos];
-    //     console.log("17");
-    // }
-    // console.log("18");
     
+    // 이 함수는 todos의 목록아이디값을 찾아서 
+    // const a = todos.find(todo => todo.id === 목록아이디값);
+    
+    // 그 목록아이디값 제외한 배열을 새로 만든후
+
+    
+    //  그 배열이 새로운 todos 상태값이 된다.
+
+    //  setTodos(새로운배열)
+
 
 
     return (
         <>
             <div className="todoList-wrap">
+                
                 <input type= "text" onChange={ inputChange } value={ tdInput }></input>
                 <button onClick={ addTodoBtn }>추가</button>
             </div>
             <ul>
+                
                 {
                     todos.map( 
                         todo => 
                         <li key={ todo }> 
                             <p> {todo} </p>
-                            {/* <button onClick={ () => moveCompleteBtn(todo) }>Complete</button> */}
                             <span>
                                 <button onClick={ () => deleteTodoBtn(todo) } >Delete</button>
                             </span>
                         </li>
                     )
                 }
-                <li>
-                    <p>adfasdfasdf</p>
-                    <span>
-                        <button>Delete</button>
-                    </span>
-                </li>
             </ul>
         </>
     );
@@ -166,3 +160,26 @@ export default TodoList;
     /// 룬 예상
     // todos = ["코드깎는노인 구독하기", "제로초 구독하기", ["위구룬", "공부하기", "퇴근은없다"]];
     // todos2 = ["코드깎는노인 구독 취소하기", "제로초 구독하기", ["위구룬", "공부하기", "퇴근은없다"]];
+
+
+
+    // 이동
+    // const todoComplete = querySelector("#todoCompleteId");
+    // console.log("15");
+
+    // function moveCompleteBtn(todo) {
+    //     console.log("16");
+    //     todoComplete = [...todos];
+    //     console.log("17");
+    // }
+    // console.log("18");
+
+
+
+                // {/* <li>
+                //     <p>adfasdfasdf</p>
+                //     <span>
+                //         <button>Delete</button>
+                //     </span>
+                // </li> */}
+                // {/* <button onClick={ () => moveCompleteBtn(todo) }>Complete</button> */}
